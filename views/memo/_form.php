6226427e042936138a41a2ee3cd262f93b12f80e
beerfleet <jvbiervliet@gmail.com>
2026-09-08 15:50:52 +0200

    added styling to button

--- views/memo/_form.php
+++ views/memo/_form.php
@@ -18,7 +18,7 @@ use yii\widgets\ActiveForm;
 
     <?= $form->field($model, 'type_id')->textInput() ?>
 
-    <div class="form-group">
+    <div class="form-group mt-3">
         <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
     </div>
 
